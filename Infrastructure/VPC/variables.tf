b2dd8abbcbf34c0419c9bb88b97b1a45f1a61a5b
junaid-13 <zufishanjunaid.1999@gmail.com>
2026-01-01 09:30:05 +0530

    Written the vpc related variables for initial.

---
+++ Infrastructure/VPC/variables.tf
@@ -0,0 +1,14 @@
+variable "name" {
+type = string
+default = "Purge_db"
+}
+
+variable "cidr" {
+  type = string
+  default = "125.0.0.0/16"
+}
+
+variable "azs" {
+  type = list()
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
