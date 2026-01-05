bf8f3ef058bfbb9797109e6c9655feb15837c3cc
junaid-13 <zufishanjunaid.1999@gmail.com>
2026-01-05 12:15:26 +0530

    Added the other vpc resources and fixed the errors.

--- Infrastructure/VPC/main.tf
+++ Infrastructure/VPC/main.tf
@@ -1,32 +1,59 @@
 data "aws_region" "current" {}
 
 locals {
-  pub_sub = ["10.0.1.0/24", "10.0.3.0/24", "10.0.5.0/24"]
-  pri_sub = ["10.0.50.0/24", "10.0.51.0/24", "10.0.52.0/24"]
+  pub_sub = {
+    0 = "10.0.1.0/24"
+    1 = "10.0.3.0/24"
+    2 = "10.0.5.0/24"
+  }
+  pri_sub = {
+    0 = "10.0.50.0/24"
+    1 = "10.0.51.0/24"
+    2 = "10.0.52.0/24"
+  }
 }
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
-  name = "${var.name}-vpc"
-  cidr = var.cidr
+  name   = "${var.name}-vpc"
+  cidr   = var.cidr
 }
 
 resource "aws_subnet" "pub_sub" {
-    for_each = local.pub_sub
-    vpc_id = module.vpc.id
-    availability_zone = var.azs[each.value]
-    cidr_block = each.value
-    tags = {
-  name = for_each["${var.name}-public-subnet-${each.key}"]
-}
+  for_each          = local.pub_sub
+  vpc_id            = module.vpc.vpc_id
+  availability_zone = var.azs[each.key]
+  cidr_block        = each.value
+  tags = {
+    name = "${var.name}-public-subnet-${each.key}"
+  }
 }
 
 resource "aws_subnet" "pri_sub" {
-  for_each = local.pri_sub
-  vpc_id = module.vpc.id
-  availability_zone = var.azs[each.value]
-  cidr_block = each.value
+  for_each          = local.pri_sub
+  vpc_id            = module.vpc.vpc_id
+  availability_zone = var.azs[each.key]
+  cidr_block        = each.value
   tags = {
-    name = for_each["${var.name}-private-subnet-${each.key}"]
+    name = "${var.name}-private-subnet-${each.key}"
+  }
+}
+
+resource "aws_internet_gateway" "aws_igw" {
+  vpc_id = module.vpc.vpc_id
+  tags = {
+    Name = "${var.name}-igw"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = module.vpc.vpc_id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.aws_igw.id
+  }
+
+  tags = {
+    name = "${var.name}-public-rt"
   }
 }
